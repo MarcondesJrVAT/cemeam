cefb0acc98eb313b936279974870d9b016fb09ab
MarcondesJrVAT <marcondes.junio@vat.com.br>
2024-04-03 11:39:36 -0400

    Nomeação de rota

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('site.dashboard');
-});
+})->name('site.dashboard');
 
 Route::middleware([
     'auth:sanctum',
