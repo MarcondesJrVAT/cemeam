cb06958d7e99cb9d8baa73f5aa82470eee383286
MarcondesJrVAT <marcondes.junio@vat.com.br>
2024-04-03 16:47:15 -0400

    Geração de 10 usuarios e 1 usuario padrao para login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Super',
+            'email' => 'super@admin.com',
+            'password' => bcrypt('password'),
         ]);
     }
 }
