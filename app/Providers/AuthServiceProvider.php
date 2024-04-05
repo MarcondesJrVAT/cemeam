d9dc24d07f983b463e44ca6ae61296a3309836a6
MarcondesJrVAT <marcondes.junio@vat.com.br>
2024-04-05 09:16:59 -0400

    remoção de comentarios e inclusão de Gates para autorização de funções

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,29 +2,35 @@
 
 namespace App\Providers;
 
+use App\Models\Permission;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap services.
-     */
-    public function boot()
+    public function boot(): void
     {
+        if (Schema::hasTable('permissions')) {
+            $permissions = Permission::all();
+            foreach ($permissions as $permission) {
+                Gate::define($permission->name, static function (User $user) use ($permission) {
+                    return $user->hasPermission($permission);
+                });
+            }
+        }
+        
         Gate::before(static function (User $user, $permission) {
             if ($user->permissions()->contains($permission)) {
                 return true;
             }
+            return null;
         });
     }
 }
