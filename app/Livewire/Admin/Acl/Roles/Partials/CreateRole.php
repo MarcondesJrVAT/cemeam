76380b8d2212c78cc84c826b45c1d0d894bf184c
MarcondesJrVAT <marcondes.junio@vat.com.br>
2024-04-08 15:00:53 -0400

    Implementando o componente Livewire de Criar Funções

---
+++ app/Livewire/Admin/Acl/Roles/Partials/CreateRole.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Livewire\Admin\Acl\Roles\Partials;
+
+use App\Models\Role;
+use Illuminate\Support\Str;
+use Illuminate\View\View;
+use Livewire\Component;
+
+class CreateRole extends Component
+{
+    public bool $showModal = false;
+    public string $name;
+
+    public function toggleModal(): void
+    {
+        $this->resetValidation();
+        $this->showModal = !$this->showModal;
+    }
+
+    public function render(): View
+    {
+        return view('livewire.admin.acl.roles.partials.create-role');
+    }
+
+    public function submit()
+    {
+        $this->validate([
+            'name' => 'required|string|max:255|unique:roles,name',
+        ]);
+
+        $validatedData = [
+            'name' => $this->name,
+            'slug' => Str::slug($this->name),
+        ];
+
+        Role::create($validatedData);
+
+        session()->flash('success', 'Role Criada com Sucesso!');
+
+        $this->toggleModal();
+
+        return redirect()->route('admin.roles');
+    }
+}
