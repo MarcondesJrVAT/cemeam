4fddbd1600c40177e852316e6354313fc0d6705b
MarcondesJrVAT <marcondes.junio@vat.com.br>
2024-04-08 15:01:50 -0400

    Implementando o componente Livewire para Criar Usuários

---
+++ app/Livewire/Admin/Acl/Users/Partials/CreateUser.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Livewire\Admin\Acl\Users\Partials;
+
+use App\Models\User;
+use Livewire\Component;
+
+class CreateUser extends Component
+{
+    public $showModal = false;
+    public $name;
+    public $email;
+    public $password;
+    public $password_confirmation;
+
+    public function toggleModal(): void
+    {
+        $this->resetValidation();
+        $this->showModal = !$this->showModal;
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.acl.users.partials.create-user');
+    }
+
+    public function submit()
+    {
+        $validatedData = $this->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed',
+            'password_confirmation' => 'required',
+        ]);
+
+        $validatedData['password'] = bcrypt($validatedData['password']);
+
+        User::create($validatedData);
+
+        session()->flash('message', 'Usuário Criado com Sucesso.');
+
+        $this->toggleModal();
+
+        return redirect()->route('admin.users');
+    }
+}
