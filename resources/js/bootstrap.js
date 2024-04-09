6a9e51a8565d04d8b5271892991c269dc52ae55a
MarcondesJrVAT <marcondes.junio@vat.com.br>
2024-04-09 07:16:53 -0400

    importação de componentes para funcionamento do select2

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,11 @@
 import axios from 'axios';
+
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+
+import select2 from 'select2';
+select2();
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
